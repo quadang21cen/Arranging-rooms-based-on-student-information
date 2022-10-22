1269b5012e30b55af72065978835916e3c1943c4
dung1308 <90315963+dung1308@users.noreply.github.com>
2022-10-22 23:25:19 +0700

    Update TF-IDF/main.py
    Remove Punctuation function added

--- NLP/TF_IDF/main.py
+++ NLP/TF_IDF/main.py
@@ -1,6 +1,6 @@
-from underthesea import sent_tokenize, text_normalize, word_tokenize
+from underthesea import text_normalize, word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import string
 def get_stopwords_list(stop_file_path):
     """load stop words """
     
@@ -9,19 +9,29 @@ def get_stopwords_list(stop_file_path):
         stop_set = set(m.strip() for m in stopwords)
         return list(frozenset(stop_set))
 def tokenize_vn(doc):
+    # Remove Numbers (Dung de xoa so)
+    # doc = re.sub(r'\d+', '', doc)
+
+    punctuation = string.punctuation
     normalized_doc = text_normalize(doc)
     doc = word_tokenize(normalized_doc)
-    return doc
+    # Remove Punctuation (VD: Xoa dau !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ )
+    clean_words = [w for w in doc if w not in punctuation]
+    return clean_words
 
 
-
-
-corpus = ["tôi thích bơi lội,nghe nhạc, và đọc sách", 
-           ""
+corpus = ["tôi  thích bơi lội,nghe nhạc, và đọc sách",
+            "Toi thich da bong"
            ]      
-stop_words_list = get_stopwords_list("C:\\Users\\quach\\Desktop\\Recent\\RS_demo\\vietnamese_stopwords.txt")                                                                                                                                                                                             
-vect = TfidfVectorizer(tokenizer=tokenize_vn, stop_words=stop_words_list)                                                                                                                                                                                                   
-tfidf = vect.fit_transform(corpus)                                                                                                                                                                                                                       
+stop_words_list = get_stopwords_list(".\\vietnamese_stopwords.txt")
+vect = TfidfVectorizer(tokenizer=tokenize_vn, stop_words=stop_words_list, lowercase = True)
+tfidf = vect.fit_transform(corpus)
+
+import pandas as pd
+feature_df = pd.DataFrame(tfidf.toarray(),
+                        columns=vect.get_feature_names_out())
+print(feature_df)
+
 pairwise_similarity = tfidf * tfidf.T 
 
-print(pairwise_similarity)
+print("Do tuong dong:",pairwise_similarity)
