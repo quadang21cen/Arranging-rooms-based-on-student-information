817d63f93120f7406b0bf4115d7b6214542e60e2
quadang21cen <quadang21cen@gmail.com>
2022-11-06 14:41:14 +0700

    ADD PhoBERT

--- Recomender/phoBert/main.py
+++
@@ -1,3 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
-model = SentenceTransformer('C:\\Users\\quach\\Desktop\\Personal\\FPT University\\SEMESTER 9\\vn_sbert_deploy\\phobert_base_mean_tokens_NLI_STS')
