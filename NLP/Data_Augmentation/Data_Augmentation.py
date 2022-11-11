c64400cf961d96862bd717a799c7f2c4cd009974
dung1308 <dungnguyentien138@gmail.com>
2022-11-11 14:09:28 +0700

    Sửa augmentation

--- NLP/Data_Augmentation/Data_Augmentation.py
+++ NLP/Data_Augmentation/Data_Augmentation.py
@@ -6,11 +6,12 @@ class StringAugmentation:
         self.results = []
     def split_tokens(self, list_words, list_of_splits):
         # split each element in list and flatten list
-        result_tokens = None
+        result_tokens = list_words
         for split_string in list_of_splits:
-            if any(split_string in word for word in list_words):
-                result_tokens = [(item.lower()).split(split_string) for item in list_words]
+            if any(split_string in word for word in result_tokens):
+                result_tokens = [(item.lower()).split(split_string) for item in result_tokens]
                 result_tokens = [item for l in result_tokens for item in l]
+
         return result_tokens
     def augment(self, list_text):
         list_of_splits = [",", "và", "vừa"]
@@ -19,7 +20,7 @@ class StringAugmentation:
 
         return tokens
 
-list_samples = ["abc", "abc,bde,èg", "quà, na, ngọc "]
+list_samples = ["abc", "abc,bde,èg", "quà, na, ngọc và hạ"]
 augment = StringAugmentation()
 results = augment.augment(list_samples)
 print(results)
